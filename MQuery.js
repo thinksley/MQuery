fd70c951b34b6160244e95d3c85ee1ed86d45d95
thinksLey <939480781@qq.com>
2013-03-11 16:51:21 +0800

    Update MQuery.js
    重新添加了每个函数的注释

--- MQuery.js
+++ MQuery.js
@@ -1,3 +1,8 @@
+/**
+  *name:getByClass   通过样式名获取元素对象
+  *@param {String}   [oPar]  父集元素对象
+  *@param {String}   [sClass]   类名称
+*/
 function getByClass(oPar,sClass)
 {
   var alls=oPar.getElementsByTagName('*');
@@ -11,6 +16,13 @@ function getByClass(oPar,sClass)
   }
   return results;
 }
+
+/**
+  *name:myAddEvent   事件封装
+  *@param {Object}   [obj]   对象
+  *@param {String}   [oEv]   事件类型
+  *@param {function} [fn]    执行的函数
+*/
 function myAddEvent(obj,oEv,fn)
 {
     if(obj.attachEvent)
@@ -34,6 +46,11 @@ function myAddEvent(obj,oEv,fn)
 	}
 }
 
+/**
+  *name:getStyle      获取样式
+  *@param {Object}   [obj]   对象
+  *@param {String}   [attr]  css属性
+*/
 function getStyle(obj,attr)
 {
    if(obj.currentStyle)
@@ -45,6 +62,11 @@ function getStyle(obj,attr)
    }
 }
 
+/**
+  *name:MQuery      选择器
+  *@param {String,Object,function}   [vArg]   输入id(包括'#'),class(包括'.'),标签,对象,函数
+ 
+*/
 function MQuery(vArg)
 {
 	//保存选中元素
@@ -58,7 +80,8 @@ function MQuery(vArg)
 	        switch(vArg.charAt(0))
 			{
 			   case '#':
-			       var obj=document.getElementById(vArg.substring(1));                  this.elements.push(obj);
+			       var obj=document.getElementById(vArg.substring(1));                  
+				   this.elements.push(obj);
 			       break;
 			   case '.':
 			       this.elements=getByClass(document,vArg.substring(1));
@@ -71,6 +94,11 @@ function MQuery(vArg)
 	      this.elements.push(vArg);
 	}	
 }
+
+/**
+  *name:click          单击事件
+  *@param {function}   [fn]   执行的事件
+*/
 MQuery.prototype.click=function(fn)
 {
   	for(var i=0;i<this.elements.length;i++)
@@ -79,6 +107,10 @@ MQuery.prototype.click=function(fn)
 	}
 	return this;
 }
+
+/**
+  *name:show        元素显示
+*/
 MQuery.prototype.show=function()
 {
     for(var i=0;i<this.elements.length;i++)
@@ -87,6 +119,10 @@ MQuery.prototype.show=function()
 	}
 	return this;
 }
+
+/**
+  *name:hide        元素隐藏
+*/
 MQuery.prototype.hide=function()
 {
     for(var i=0;i<this.elements.length;i++)
@@ -95,6 +131,12 @@ MQuery.prototype.hide=function()
 	}
 	return this;
 }
+
+/**
+  *name:hover            鼠标移上和移去
+  *@param {function}     [fnOver]     鼠标移动上去的事件
+  *@param {function}     [fnOut]      鼠标移开的事件
+*/
 MQuery.prototype.hover=function(fnOver,fnOut)
 {
   	for(var i=0;i<this.elements.length;i++)
@@ -104,6 +146,12 @@ MQuery.prototype.hover=function(fnOver,fnOut)
 	}
 	return this;
 }
+
+/**
+  *name:css             获取css和设置css
+  *@param {String}     [attr]          属性
+  *@param {String}     [value]         值，可有可无，当无时表示获取css值，写了表示重置css值
+*/
 MQuery.prototype.css=function(attr,value)
 {
     if(arguments.length==2) //设置样式
@@ -133,6 +181,11 @@ MQuery.prototype.css=function(attr,value)
 	return this;
 }
 
+/**
+  *name:css              获取css和设置css
+  *@param {String}       [attr]          属性
+  *@param {String}       [value]         值，可有可无，当无时表示获取css值，写了表示重置css值
+*/
 MQuery.prototype.attr=function(attr,value)
 {
     if(arguments.length==2) //设置
@@ -148,6 +201,10 @@ MQuery.prototype.attr=function(attr,value)
 	return this;
 }
 
+/**
+  *name:toggle              单击一次就轮流执行里面的参数函数
+  *@param {function}
+*/
 MQuery.prototype.toggle=function()
 {
 	var _arguments=arguments;
@@ -165,11 +222,18 @@ MQuery.prototype.toggle=function()
 	return this;
 }
 
+/**
+  *name:eq              获取第几个元素
+  *@param {Number}      [n]
+*/
 MQuery.prototype.eq=function(n)
 {
    return $(this.elements[n]);
 }
 
+/**
+  *name:appendArr            for MQuery.prototype.find
+*/
 function appendArr(arr1,arr2)
 {
   for(var i=0;i<arr2.length;i++)
@@ -178,6 +242,10 @@ function appendArr(arr1,arr2)
   }
 }
 
+/**
+  *name:find     查找元素
+  *@param {String}  [str]  类名包括'.'  或标签
+*/
 MQuery.prototype.find=function(str)
 {
    var aResult=[];
@@ -199,6 +267,9 @@ MQuery.prototype.find=function(str)
 	return newMQuery;
 }
 
+/**
+  *name:getIndex            for MQuery.prototype.index
+*/
 function getIndex(obj)
 {
    var oBrother=obj.parentNode.children;
@@ -210,6 +281,10 @@ function getIndex(obj)
 	  }  
    }	
 }
+
+/**
+  *name:index       查找同行子元素的索引号
+*/
 MQuery.prototype.index=function()
 {
    	return getIndex(this.elements[0]);
@@ -217,6 +292,9 @@ MQuery.prototype.index=function()
 
 /*******************/
 /*使用bind时候要想阻止默认事件和事件冒泡的话 得加return false；其具体方法实现在myAddEvent里面实现
+ *name: bind          绑定事件
+ *@param {String}     [sEv]   事件名称
+ *@param {function}   [fn]    执行的函数
 */
 /*******************/
 
@@ -240,6 +318,10 @@ MQuery.prototype.extend=function(name,fn) //插件机制
   MQuery.prototype[name]=fn;
 }
 
+/**
+实例化一个MQuery对象
+如：$('#div');  $('.class');  $(function(){alert('hello world')});  $('div');
+*/
 function $(vArg)
 {
   return new MQuery(vArg);

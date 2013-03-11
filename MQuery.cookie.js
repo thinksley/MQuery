19d054f32f6aa1037a84fd7c82b7d008ba00c29a
thinksLey <939480781@qq.com>
2013-03-11 16:20:05 +0800

    Create MQuery.cookie.js
    添加了设置，获取，删除cookie的插件 date:2013年3月11日 16:20:12

---
+++ MQuery.cookie.js
@@ -0,0 +1,36 @@
+/**
+ *name:  setCookie  设置cookie
+ *@param {String}   [name]   cookie名称
+ *@param {String}   [value]  cookie值
+ *@param {Number}   [iDay]   过期时间
+*/
+$().extend('setCookie',function(name,value,iDay){ 
+      var date=new Date();
+		date.setDate(date.getDate()+iDay);
+		document.cookie=name+'='+value+';expires='+date;
+});
+
+/**
+ *name:  getCookie  获取cookie
+ *@param {String}   [name]   cookie名称
+*/
+$().extend('getCookie',function(name){ 
+	  var arr=document.cookie.split('; ');
+	  for(var i=0;i<arr.length;i++)
+	  {
+		  var temp=arr[i].split('=');
+		  if(temp[0]==name)
+		  {
+			    return temp[1];
+		  }
+	  }
+	  return '';
+});
+
+/**
+ *name:removeCookie  删除cookie
+ *@param {String}    [name]   cookie名称
+*/
+$().extend('removeCookie',function(name){ 
+	    $().setCookie(name,'1',-1);
+});
